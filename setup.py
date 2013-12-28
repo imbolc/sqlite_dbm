4059d20e93fdb7d353dee6315773290cbde78939
imbolc <imbolc@imbolc.name>
2013-12-28 14:14:25 +0400

    v1.0.0

---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+import os
+import sys
+import doctest
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import sqlite_dbm as mod
+
+
+DOC = mod.__doc__.strip()
+
+open('README.md', 'w').write(DOC)
+if sys.argv[-1] == 'publish':
+    if not doctest.testfile('README.md', verbose=True).failed:
+        os.system('python setup.py sdist upload')
+        sys.exit(0)
+
+setup(
+    name=mod.__name__,
+    url='https://github.com/imbolc/%s' % mod.__name__,
+    version=mod.__version__,
+    description=DOC.split('===\n')[1].strip().split('\n\n')[0],
+    long_description=DOC,
+
+    py_modules=[mod.__name__],
+
+    author='Imbolc',
+    author_email='imbolc@imbolc.name',
+    license='ISC',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: ISC License (ISCL)',
+    ],
+)
---
+++ sqlite_dbm.py
@@ -0,0 +1,89 @@
+'''
+sqlite-dbm
+==========
+Dict-style DBM based on sqlite3.
+
+    >>> import sqlite_dbm
+    >>> db = sqlite_dbm.open('./test.sqlite')
+    >>> db['foo'] = 'bar'
+    >>> db['foo']
+    u'bar'
+    >>> del db['foo']
+    >>> len(db)
+    0
+    >>> db.close()
+
+Fast insert:
+
+    >>> db = sqlite_dbm.open('./test.sqlite', auto_commit=False)
+    >>> for i in xrange(1000):
+    ...     db[str(i)] = 'foo'
+    >>> db.commit()
+    >>> len(db)
+    1000
+    >>> db.clear()
+    >>> db.close()
+'''
+import sqlite3
+from UserDict import DictMixin
+
+
+__version__ = '1.0.0'
+
+
+class SQLiteDBM(DictMixin):
+    def __init__(self, filename, auto_commit=True):
+        self.filename = filename
+        self.auto_commit = auto_commit
+        self.open()
+
+    def open(self):
+        self.conn = sqlite3.connect(self.filename)
+        self.cur = self.conn.cursor()
+        self.create_table()
+
+    def create_table(self):
+        self.cur.execute(
+            '''CREATE TABLE IF NOT EXISTS kv (
+                id    VARCHAR PRIMARY KEY,
+                value VARCHAR
+            )''')
+        self.conn.commit()
+
+    def commit(self):
+        self.conn.commit()
+
+    def close(self):
+        self.conn.execute("VACUUM")
+        self.conn.close()
+
+    def __getitem__(self, key):
+        self.cur.execute('SELECT * FROM kv WHERE id=?', (key, ))
+        data = self.cur.fetchone()
+        if not data:
+            raise KeyError(key)
+        return data[1]
+
+    def __setitem__(self, key, value):
+        self.cur.execute(
+            'REPLACE INTO kv (id, value) VALUES (?, ?)', (key, value))
+        if self.auto_commit:
+            self.conn.commit()
+
+    def __delitem__(self, key):
+        self.cur.execute('DELETE FROM kv WHERE id=?', (key, ))
+        if self.auto_commit:
+            self.conn.commit()
+
+    def keys(self):
+        self.cur.execute('SELECT id FROM kv')
+        return [r[0] for r in self.cur.fetchall()]
+
+
+def open(*args, **kwargs):
+    return SQLiteDBM(*args, **kwargs)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(verbose=True)
